3a3f0ffaac0c83c0ca44fc9661d81d444a87deb1
Aniket <01aniket26@gmail.com>
2023-06-25 17:09:17 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { logout } from "./shared/utils/auth";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: process.env.URL,
   timeout: 1000,
 });
 
